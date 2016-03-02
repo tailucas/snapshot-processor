969faabc2adcccbda5a4289de73eeb7f1cf1cded
db2inst1 <db2inst1@webafrica.org.za>
2016-03-02 18:04:24 +0200

    Move awslogs-agent-setup into a script that noops if not required.

---
+++ awslogs-agent-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+if [ ! -f /var/awslogs/etc/awslogs.conf ]; then
+  wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O /app/awslogs-agent-setup.py
+  python /app/awslogs-agent-setup.py -n -r "eu-west-1" -c /app/config/awslogs-config
+  # remove the service and nanny (supervisor does this)
+  update-rc.d awslogs remove
+  rm -f /etc/cron.d/awslogs
+fi
