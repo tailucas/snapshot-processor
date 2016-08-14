f3309b1ad4eb14edfe3de5c4ad1854da19fc4434
db2inst1 <db2inst1@webafrica.org.za>
2016-08-14 22:11:31 +0200

    trap signals for supervisor

--- entrypoint.sh
+++ entrypoint.sh
@@ -167,4 +167,8 @@ export HOME=/data/
 
 # I'm the supervisor
 cat /app/config/supervisord.conf | python /app/config_interpol | tee /etc/supervisor/conf.d/supervisord.conf
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+trap 'kill -TERM $PID; wait $PID; exit $?' TERM INT HUP
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf &
+PID=$!
+wait $PID
