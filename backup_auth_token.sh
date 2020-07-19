cc3603c5fa0fe45a90b59ba36ca8364eefeac606
Tai Lucas <tglucas@gmail.com>
2020-07-19 07:31:40 +0200

    Move auth token cache location.

--- backup_auth_token.sh
+++ backup_auth_token.sh
@@ -5,8 +5,8 @@ set -o pipefail
 . <(cat /opt/app/environment.env | sed 's/^/export /')
 . /opt/app/bin/activate
 if [ -f /data/snapshot_processor_creds ]; then
-  aws s3 cp /data/snapshot_processor_creds s3://tailucas/home-automation/snapshot_processor_creds --only-show-errors
+  aws s3 cp /data/snapshot_processor_creds s3://tailucas-automation/snapshot_processor_creds --only-show-errors
 else
-  aws s3 cp s3://tailucas/home-automation/snapshot_processor_creds /data/snapshot_processor_creds --only-show-errors
+  aws s3 cp s3://tailucas-automation/snapshot_processor_creds /data/snapshot_processor_creds --only-show-errors
 fi
 deactivate
