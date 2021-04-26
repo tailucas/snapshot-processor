8711fcf6dfc557d257f64c85e17702a5624262ba
Tai Lucas <tglucas@gmail.com>
2021-04-26 22:22:02 +0200

    Fix auth lookup for bash tool.

--- backup_auth_token.sh
+++ backup_auth_token.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
-# may be run from the context of the entrypoint for first-run bootstrap.
-if [ -f /opt/app/environment.env ]; then
-  . <(cat /opt/app/environment.env | sed 's/^/export /')
-fi
+
+. <(cat /opt/app/environment.env | sed 's/^/export /')
+# pip-installed aws cli
 . /opt/app/bin/activate
-AWS_ACCESS_KEY_ID="$(/opt/app/bin/python /opt/app/cred_tool <<< '{"s": {"opitem": "AWS", "opfield": "${AWS_DEFAULT_REGION}.akid"}}')"
-AWS_SECRET_ACCESS_KEY="$(/opt/app/bin/python /opt/app/cred_tool <<< '{"s": {"opitem": "AWS", "opfield": "${AWS_DEFAULT_REGION}.sak"}}')"
+AKID="{\"s\": {\"opitem\": \"AWS\", \"opfield\": \"${AWS_DEFAULT_REGION}.akid\"}}"
+export AWS_ACCESS_KEY_ID="$(/opt/app/bin/python /opt/app/cred_tool <<< "${AKID}")"
+SAK="{\"s\": {\"opitem\": \"AWS\", \"opfield\": \"${AWS_DEFAULT_REGION}.sak\"}}"
+export AWS_SECRET_ACCESS_KEY="$(/opt/app/bin/python /opt/app/cred_tool <<< "${SAK}")"
 if [ -f /data/snapshot_processor_creds ]; then
   aws s3 cp /data/snapshot_processor_creds s3://tailucas-automation/snapshot_processor_creds --only-show-errors
 else
