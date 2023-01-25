76ff43d1b7604939cf8504e72f2b64902c2904ca
Tai Lucas <tglucas@gmail.com>
2023-01-25 20:52:21 +0000

    Fix non-compliant sh syntax.

--- backup_auth_token.sh
+++ backup_auth_token.sh
@@ -6,9 +6,9 @@ set -o pipefail
 # pip-installed aws cli
 . /opt/app/bin/activate
 AKID="{\"s\": {\"opitem\": \"AWS\", \"opfield\": \"${AWS_DEFAULT_REGION}.akid\"}}"
-export AWS_ACCESS_KEY_ID="$(/opt/app/bin/python /opt/app/pylib/cred_tool <<< "${AKID}")"
+export AWS_ACCESS_KEY_ID="$(echo "${AKID}" | /opt/app/bin/python /opt/app/pylib/cred_tool)"
 SAK="{\"s\": {\"opitem\": \"AWS\", \"opfield\": \"${AWS_DEFAULT_REGION}.sak\"}}"
-export AWS_SECRET_ACCESS_KEY="$(/opt/app/bin/python /opt/app/pylib/cred_tool <<< "${SAK}")"
+export AWS_SECRET_ACCESS_KEY="$(echo "${SAK}" | /opt/app/bin/python /opt/app/pylib/cred_tool)"
 if [ -f /data/snapshot_processor_creds ]; then
   aws s3 cp /data/snapshot_processor_creds s3://tailucas-automation/snapshot_processor_creds --only-show-errors
 else
